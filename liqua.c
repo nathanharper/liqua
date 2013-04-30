638232f245e378286ef7c5c6b5588f087675db2f
Nathan Harper <nathan.sam.harper@gmail.com>
2013-04-30 14:22:35 -0400

    some incomplete updates that I dont feel like describing

--- liqua.c
+++ liqua.c
@@ -24,6 +24,7 @@ static int liqua_attr_destroy(lua_State *L)
 {
     liq_attr **attr = checkattr(L);
     liq_attr_destroy(*attr);
+    lua_pop(L, 1);
     return 0;
 }
 
@@ -31,6 +32,7 @@ static int liqua_image_destroy(lua_State *L)
 {
     liq_image **image = checkimage(L);
     liq_image_destroy(*image);
+    lua_pop(L, 1);
     return 0;
 }
 
@@ -38,6 +40,7 @@ static int liqua_result_destroy(lua_State *L)
 {
     liq_result **result = checkresult(L);
     liq_result_destroy(*result);
+    lua_pop(L, 1);
     return 0;
 }
 
@@ -48,21 +51,21 @@ static int liqua_result_destroy(lua_State *L)
 
 static int liqua_attr_tostring(lua_State *L)
 {
-    /* checkattr(L); */
+    luaL_checkudata(L, 1, "liqua.attr");
     lua_pushstring(L, "<liqua attribute>");
     return 1;
 }
 
 static int liqua_image_tostring(lua_State *L)
 {
-    /* checkimage(L); */
+    luaL_checkudata(L, 1, "liqua.image");
     lua_pushstring(L, "<liqua image>");
     return 1;
 }
 
 static int liqua_result_tostring(lua_State *L)
 {
-    /* checkresult(L); */
+    luaL_checkudata(L, 1, "liqua.result");
     lua_pushstring(L, "<liqua result>");
     return 1;
 }
@@ -75,14 +78,9 @@ static int liqua_result_tostring(lua_State *L)
 
 static int liqua_attr_create(lua_State *L)
 {
-    liq_attr *attr = liq_attr_create();
-
-    if (attr == NULL) {
-        lua_pushnil(L);
-        lua_pushstring(L, "Failed to initialize liq attribute.");
-        return 2;
-    }
-
+    liq_attr *attr;
+    if ((attr = liq_attr_create()) == NULL)
+        return liqua_push_error(L, "Failed to initialize liqua attribute.");
     liq_attr **ud = (liq_attr *)lua_newuserdata(L, sizeof(attr));
     *ud = attr;
     luaL_getmetatable(L, "liqua.attr");
@@ -107,19 +105,51 @@ static int liqua_image_create_rgba(lua_State *L)
         {255,255,255,255}
     };
 
-    liq_image *image = liq_image_create_rgba(*attr, list, w, h, gamma);
-
-    if (image == NULL) {
-        lua_pushnil(L);
-        lua_pushstring(L, "Failed to create RGBA image.");
-        return 2;
-    }
+    liq_image *image;
+    if ((image = liq_image_create_rgba(*attr, list, w, h, gamma)) == NULL)
+        return liqua_push_error(L, "Failed to create RGBA image.");
 
     liq_image **ud = (liq_image *)lua_newuserdata(L, sizeof(image));
     *ud = image;
     luaL_getmetatable(L, "liqua.image");
     lua_setmetatable(L, -2);
     return 1;
+}
+
+static int liqua_quantize_image(lua_State *L)
+{
+    liq_attr **attr = checkattr(L);
+    liq_image **image = (liq_image **)luaL_checkudata(L, 2);
+    liq_result *result;
+    if ((result = liq_quantize_image(*attr, *image)) == NULL)
+        return liqua_push_error(L, "Image quantization failed.");
+    liq_result **ud = (liq_result *)lua_newuserdata(L, sizeof(result));
+    *ud = result;
+    luaL_getmetatable(L, "liqua.result");
+    lua_setmetatable(L, -2);
+    return 1;
+}
+
+static int liqua_write_remapped_image(lua_State *L)
+{
+    liq_result **result = checkresult(L);
+    liq_image **image = (liq_image **)luaL_checkudata(L, 2);
+    switch(liq_write_remapped_image(*result, *image, buffer, buffer_size)) {
+        case LIQ_OK:
+            break;
+        case LIQ_BUFFER_TOO_SMALL:
+            return liqua_push_error(L,"Buffer too small to write image.");
+        default:
+            return liqua_push_error(L,"Something went wrong while writing image but I don't know what?");
+    }
+    return 0;
+}
+
+int liqua_push_error(lua_State *L, const char[] msg)
+{
+    lua_pushnil(L);
+    lua_pushstring(L, msg);
+    return 2;
 }
 
 
@@ -136,6 +166,7 @@ static const struct luaL_Reg liqua [] = {
 static const struct luaL_Reg liqua_attr [] = {
     {"destroy", liqua_attr_destroy},
     {"image_create_rgba", liqua_image_create_rgba},
+    {"quantize_image", liqua_quantize_image},
     {"__tostring", liqua_attr_tostring},
     {NULL, NULL}
 };
@@ -149,6 +180,7 @@ static const struct luaL_Reg liqua_image [] = {
 static const struct luaL_Reg liqua_result [] = {
     {"destroy", liqua_result_destroy},
     {"__tostring", liqua_result_tostring},
+    {"write_remapped_image", liqua_write_remapped_image},
     {NULL, NULL}
 };
 
